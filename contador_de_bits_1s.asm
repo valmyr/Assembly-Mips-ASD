835774b5fdf3a6cfccc6fe77ab68d0ec95e15427
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2023-04-15 08:16:26 -0300

    Update contador_de_bits_1s.asm

--- contador_de_bits_1s.asm
+++ contador_de_bits_1s.asm
@@ -6,7 +6,7 @@ addi $s0, $zero, 0      ; iniciando $s0 com zero
 addi $s1, $s0, 1        ; $s1 entrada. O número que vamos contar os 1s
 addi $s2, $s0, 0        ; iniciando $s2 com zero. saida: número de 1s 
 addi $s3, $s0, 1        ; $s3 será o valor que efetuarei os deslocamentos para a máscara
-and  $s4, $s3, $s1      ; $s4 máscara. retorna 1 se o valor do número em $s1 for 1 e zero c.c.
+and  $s4, $s3, $s1      ; $s4 máscara. retorna 1 se o valor do número em $s1[i] for 1 e zero c.c.
 
 beq  $s4, $s0, p        ;caso o que estiver em $4 for 1 incremento à saída. c.c pula para p
 addi $s2, $s2, 1
